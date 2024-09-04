780803ffefdc6f7b40a0bfbf8f4a02ac2dbdeceb
Trayan Azarov <trayan.azarov@amikos.tech>
2024-09-04 17:38:43 +0300

    feat: Testing windows build

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -4,6 +4,7 @@ import shutil
 import subprocess
 import sys
 from pathlib import Path
+from typing import List
 
 from setuptools import Extension, setup, glob
 from setuptools.command.build_ext import build_ext
@@ -12,47 +13,59 @@ from setuptools.command.sdist import sdist
 from wheel._bdist_wheel import bdist_wheel
 from auditwheel.repair import repair_wheel
 
+
 def get_lib_name():
     if platform.system() == "Darwin":
         return "libllama-embedder.dylib"
     elif platform.system() == "Linux":
         return "libllama-embedder.so"
     elif platform.system() == "Windows":
-        # return ["llama-embedder.dll","llama-embedder.lib"]
-        return "llama-embedder.dll"
+        return ["llama-embedder.dll", "llama-embedder.lib"]
     else:
         raise OSError(f"Unsupported operating system: {platform.system()}")
 
 
+def find_and_copy_win_shared_lib(build_dir, files_to_find: List[str], target_dir: str):
+    for root, dirs, files in os.walk(build_dir):
+        for file in files_to_find:
+            if file in files:
+                source_file = os.path.join(root, file)
+                target_file = os.path.join(target_dir, file)
+                shutil.copy2(source_file, target_file)
+                print(f"Copied {source_file} to {target_file}")
+
+
 class CustomBuildExt(build_ext):
     def run(self):
+        ct = self.compiler.compiler_type
         print("doing CustomBuildExt")
-        shared_lib_path = os.path.join('build', get_lib_name())
-        print(f"Looking for shared library at: {shared_lib_path}")
+        if ct == "unix":
+            shared_lib_path = os.path.join('build', get_lib_name())
+            print(f"Looking for shared library at: {shared_lib_path}")
 
-        if not os.path.exists(shared_lib_path):
-            raise FileNotFoundError(f"Shared library not found at {shared_lib_path}")
-
+            if not os.path.exists(shared_lib_path):
+                raise FileNotFoundError(f"Shared library not found at {shared_lib_path}")
+            shutil.copy2(shared_lib_path, self.build_lib)
+        elif ct == "msvc":
+            find_and_copy_win_shared_lib("build", get_lib_name(), self.build_lib)
         # dest_path = os.path.join(self.build_lib)
         # os.makedirs(dest_path, exist_ok=True)
-        shutil.copy2(shared_lib_path, self.build_lib)
-        print(os.listdir(self.build_lib))
-        print(os.path.abspath(self.build_lib))
         # self.copy_file(shared_lib_path, self.build_lib)
         build_ext.run(self)
-        if platform.system() == "Darwin":
+        if platform.system() == "Darwin": # post-processing of MacOS lib to fix linking
             extension_path = self.get_ext_fullpath('llama_embedder')
-            cmd = ['install_name_tool', '-change', f'@rpath/{get_lib_name()}', f'@loader_path/{get_lib_name()}', extension_path]
+            cmd = ['install_name_tool', '-change', f'@rpath/{get_lib_name()}', f'@loader_path/{get_lib_name()}',
+                   extension_path]
             subprocess.check_call(cmd)
 
     def build_extensions(self):
         ct = self.compiler.compiler_type
         opts = []
-        extra_link_args=[]
+        extra_link_args = []
         if ct == "unix":
             opts.append('-DVERSION_INFO="%s"' % self.distribution.get_version())
             opts.append("-std=c++11")
-            extra_link_args.append("-L"+self.build_lib)
+            extra_link_args.append("-L" + self.build_lib)
             if platform.system() == "Darwin":
                 opts.extend(["-stdlib=libc++", "-mmacosx-version-min=10.9"])
                 extra_link_args.append("-Wl,-rpath,@loader_path/")
@@ -69,10 +82,11 @@ class CustomBuildExt(build_ext):
             ext.extra_link_args.extend(extra_link_args)
         build_ext.build_extensions(self)
 
+
 extra_link_args = [
     "-L.",
     "-lllama-embedder",
-    ]
+]
 
 ext_modules = [
     Extension(
@@ -99,9 +113,11 @@ class CustomSdist(sdist):
 
     def make_release_tree(self, base_dir, files):
         sdist.make_release_tree(self, base_dir, files)
-        # Copy shared library to the base dir of the source distribution
-        dest = os.path.join(base_dir, get_lib_name())
-        shutil.copy2(get_lib_name(), dest)
+        if platform.system() != "Windows":
+            dest = os.path.join(base_dir, get_lib_name())
+            shutil.copy2(get_lib_name(), dest)
+        elif platform.system() == "Windows":
+            ...
         dest_src_path = os.path.join(base_dir, "src")
         shutil.copytree("src", dest_src_path, dirs_exist_ok=True)
         shutil.copy2("LICENSE.md", base_dir)
@@ -115,20 +131,17 @@ class CustomBdistWheel(bdist_wheel):
     """
 
     def run(self):
-        _shared_lib = os.path.join("build", get_lib_name())
-        print("CustomBdistWheel",os.path.abspath(self.dist_dir))
-        if not os.path.exists(_shared_lib):
-            raise FileNotFoundError(f"Shared library not found at {_shared_lib}")
-
-        # Create the llama_embedder directory in the wheel
         wheel_dir = self.dist_dir
         package_dir = os.path.join(wheel_dir, 'llama_embedder')
         os.makedirs(package_dir, exist_ok=True)
-
-        # Copy the shared library to the package directory
-        dest = os.path.join(package_dir, os.path.basename(_shared_lib))
-        shutil.copy2(_shared_lib, Path(dest).parent)
-        print(os.listdir(Path(dest).parent))
+        if platform.system() != "Windows":
+            _shared_lib = os.path.join("build", get_lib_name())
+            if not os.path.exists(_shared_lib):
+                raise FileNotFoundError(f"Shared library not found at {_shared_lib}")
+            dest = os.path.join(package_dir, os.path.basename(_shared_lib))
+            shutil.copy2(_shared_lib, Path(dest).parent)
+        elif platform.system() == "Windows":
+            find_and_copy_win_shared_lib("build", get_lib_name(), str(Path(package_dir).parent))
 
         dest_src_path = os.path.join(self.dist_dir, "src")
         shutil.copytree("src", dest_src_path, dirs_exist_ok=True)
@@ -140,9 +153,9 @@ class CustomBdistWheel(bdist_wheel):
 
 
 setup(
-    package_data={"llama_embedder": [f"{get_lib_name()}"]},
+    package_data={"llama_embedder": [f"{get_lib_name()}"] if platform.system() != "Windows" else get_lib_name()},
     include_package_data=True,
     zip_safe=False,
     ext_modules=ext_modules,
-    cmdclass={"build_ext": CustomBuildExt,"sdist": CustomSdist,"bdist_wheel": CustomBdistWheel,},
+    cmdclass={"build_ext": CustomBuildExt, "sdist": CustomSdist, "bdist_wheel": CustomBdistWheel, },
 )
