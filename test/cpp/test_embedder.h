7b9b23050fa3835aa777335fa2bbbe364a9d0851
Trayan Azarov <trayan.azarov@amikos.tech>
2024-09-11 11:59:59 +0300

    feat: Library test

---
+++ test/cpp/test_embedder.h
@@ -0,0 +1,14 @@
+//
+// Created by Trayan Azarov on 11.09.24.
+//
+
+#ifndef LLAMACPP_EMBEDDER_TEST_EMBEDDER_H
+#define LLAMACPP_EMBEDDER_TEST_EMBEDDER_H
+
+
+class test_embedder {
+
+};
+
+
+#endif //LLAMACPP_EMBEDDER_TEST_EMBEDDER_H
